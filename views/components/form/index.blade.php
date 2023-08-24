ac51613e1a41c0682187d39b79fc3ff66593a5ac
Eric Farris <eric.farris@umces.edu>
2023-08-24 15:10:33 -0400

    Fixed form.index incorrect call to x-e::alert

--- views/components/form/index.blade.php
+++ views/components/form/index.blade.php
@@ -6,7 +6,7 @@
 @endunless
 
 @if ($errors->any())
-<x-euikit.alert type="error">Your changes were not submitted due to errors in the form. Please correct the appropriate fields below.</x-euikit.alert>
+<x-euikit::alert type="error">Your changes were not submitted due to errors in the form. Please correct the appropriate fields below.</x-euikit::alert>
 @endif
 
 
