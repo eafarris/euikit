571d029baad5742f9b81374dc52b2122a726b3d2
Eric Farris <eric.farris@umces.edu>
2023-08-26 11:47:20 -0400

    dark mode support for sections and menus

--- views/components/menu/item.blade.php
+++ views/components/menu/item.blade.php
@@ -1,7 +1,7 @@
 @php
-$classes = 'p-2 pr-0 text-slate-500 hover:bg-slate-200 hover:rounded-tl-lg hover:rounded-bl-lg';
+$classes = 'p-2 pr-0 text-slate-500 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:rounded-bl-lg';
 if ($route == url()->current()) {
-    $classes = 'p-2 pr-0 text-slate-500 bg-white text-slate-500 rounded-tl-lg rounded-bl-lg';
+    $classes = 'p-2 pr-0 text-slate-500 bg-white dark:text-slate-300 dark:bg-slate-800 rounded-tl-lg rounded-bl-lg';
 }
 @endphp
 
