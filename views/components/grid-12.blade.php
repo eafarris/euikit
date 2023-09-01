e17a469b15e1a823a6913468be2e2a9a3c660245
Eric Farris <eric.farris@umces.edu>
2023-09-01 08:45:53 -0400

    grids support arbitrary classes

--- views/components/grid-12.blade.php
+++ views/components/grid-12.blade.php
@@ -1,3 +1,3 @@
-<div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-12">
+<div {{ $attributes=> merge(['class' => 'mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-12']) }}>
     {{ $slot }}
 </div><!-- EUIKit Grid 12 component -->
