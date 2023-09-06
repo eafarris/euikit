d46118c3f511a22f75ec1c8235e0489adaa07b79
Eric Farris <eric.farris@umces.edu>
2023-09-06 13:40:04 -0400

    Trying some subtle gradients

--- views/components/section.blade.php
+++ views/components/section.blade.php
@@ -1,7 +1,7 @@
 @props(['header' => '', 'footer' => ''])
-<section class="min-w-fit m-12 space-y-6 border-slate-300 bg-slate-100 dark:border-slate-900 dark:bg-slate-600 border-2 p-6 rounded-lg shadow-2xl"><!-- EUIKit Section component -->
+<section class="min-w-fit m-12 space-y-6 border-slate-300 bg-gradient-to-br from-slate-100 via-slate-200 via-20% to-slate-100 dark:border-slate-900 dark:bg-slate-600 border-2 p-6 rounded-lg shadow-2xl"><!-- EUIKit Section component -->
 @if($header)
-<div class="py-3 px-6 border-2 border-slate-300 bg-slate-200 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300 rounded-lg">
+<div class="py-3 px-6 border-2 border-slate-300 bg-slate-200 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300 rounded-lg">
     <div class="flex flex-row items-center justify-between text-2xl">
         {{ $header }}
     </div>
