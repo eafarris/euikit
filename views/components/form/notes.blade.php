c696fd89ecceb761b9fcf1bf20c4bd779675f2de
Eric Farris <eric.farris@umces.edu>
2023-09-06 16:07:29 -0400

    Fixed extra spaces in textarea for notes

--- views/components/form/notes.blade.php
+++ views/components/form/notes.blade.php
@@ -4,10 +4,9 @@
         Notes <span class="italic">(<a href="https://www.markdownguide.org/cheat-sheet/" class="underline-offset-2 underline">Markdown</a> supported)</span>
     </label>
     <div class="mt-1">
-        <textarea 
+<textarea 
         class="appearance-none block text-slate-700 dark:bg-slate-400 dark:text-slate-700 border border-slate-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-slate-500 {{ $errors->has('notes') ? 'is-danger' : ''}} "
         cols="60" rows="10" name="notes" id="notes">
-{{ $model == '' ? '' : $model->notes }}
-        </textarea>
+{{ $model == '' ? '' : $model->notes }}</textarea>
     </div>
 </div><!-- EUIKit Notes field -->
