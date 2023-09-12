0c23af7009cd64d7172cdcf13a35eac2c7df637c
Eric Farris <eric.farris@umces.edu>
2023-09-12 12:35:29 -0400

    beginnings of ledger component

---
+++ views/components/form/ledger.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <div class="grid grid-cols-12 divide-x divide-y divide-slate-400">
+        @foreach ($array as $index => $item)
+            <div @class([
+                'p-2',
+                'bg-slate-300',
+                'text-slate-500',
+                'col-span-' . $colspan => !$loop->last,
+                'col-span-' . $lastcol => $loop->last,
+            ])>
+                {{ $index }}
+            </div>
+        @endforeach
+
+        @foreach ($array as $item)
+        <input type="text" name="array[]" value="{{ $item }}" @class([
+            'col-span-' . $colspan => !$loop->last,
+            'col-span-' . $lastcol => $loop->last,
+        ]) />
+        @endforeach
+    </div><!-- .grid-cols-12 -->
+</div><!-- EUIKit Ledger Component
