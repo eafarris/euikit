d9b3304afb5a16e6c1e6580091f455219b77dce3
Eric Farris <eric.farris@umces.edu>
2023-09-12 12:35:12 -0400

    metric uses slot

--- views/components/metric.blade.php
+++ views/components/metric.blade.php
@@ -1,4 +1,4 @@
-@props(['label' => '', 'value' => ''])
+@props(['label' => ''])
 <div {{ $attributes->merge(['class' => 'bg-transparent'])}}><!-- EUIKit metric component -->
     <div class="bg-slate-100 p-4 rounded-t-xl border-slate-300 border">
         <p class="text-sm font-medium text-slate-500">
@@ -7,7 +7,7 @@
     </div><!-- .bg-slate-100 -->
     <div class="bg-slate-50 pl-4 pb-4 pt-2 rounded-b-xl border-slate-300 border-b border-l border-r">
         <p class="mt-1 text-2xl font-semibold tracking-tight text-slate-500">
-            {{ $value }}
+            {{ $slot }}
         </p>
     </div><!-- .bg-slate-100 p-4 rounded-b-xl border-slate-300 border -->
 </div>
