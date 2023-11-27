7bd8ddbeec365b889b2bc8e255d1cb7e33ecb9c0
Eric Farris <eric.farris@umces.edu>
2023-11-27 09:16:19 -0500

    More fleshing out of form.display component

--- views/components/form/display.blade.php
+++ views/components/form/display.blade.php
@@ -1,8 +1,17 @@
-@props(['label' => '', 'field', 'type' => 'text', 'value'])
-<div class="field" id="{{ $field }}">
-    <label for="{{ $field }}" class="field-label">
-    {{ $label ?: ucfirst($field) }}
-    </label>
+@props([
+    'type' => 'text', 'label' => '', 'field', 'value' => '',
+    'placeholder' => '', 'icon' => '', 'required' => FALSE, 'inline' => FALSE
+])
+@php
+    $color_classes = 'border-slate-300 dark:bg-slate-400 dark:text-slate-700 focus:border-indigo-500 focus:ring-indigo-500';
+    $error_classes = 'border-red-500 text-red-900';
+@endphp
+<div {{ $attributes->merge(['class' => 'field']) }} id="{{ $field }}">
+    @if (!$inline)
+        <label for="{{ $field }}" class="block text-sm font-medium text-slate-500 dark:text-slate-300 bg-transparent">
+            {{ $label ?: ucfirst($field) }}
+        </label>
+    @endif
     <div class="mt-1 border-none">
         @switch($type)
         @case('lookup') 
@@ -10,7 +19,9 @@
                 id="{{ $field }}"
                 name="{{ $field }}"
                 value="{{ $value }}"
-                class="text-slate-700 block w-full rounded-md border border-slate-300 sm:text-sm {{ $errors->has($field) ? 'is-danger' : ''}}"
+                class="block w-full rounded-md shadow-sm sm:text-sm placeholder:italic placeholder:text-slate-400 {{ $errors->has($field) ? $error_classes : $color_classes }} "
+                @if ($required) required @endif
+                {{ $attributes->merge(['list' => '']) }}
             />
             @break
         @case('date')
@@ -39,4 +50,4 @@
             />
         @endswitch
     </div>
-</div><!-- InsideUIKit Display Field component-->
+</div><!-- EUIKit Display Field component-->
