71f2af87715d5b17a58b90669a1ccbfe9f12e5dc
Eric Farris <eric.farris@umces.edu>
2023-11-29 09:44:09 -0500

    Default footer top margin in section component.

--- views/components/section.blade.php
+++ views/components/section.blade.php
@@ -17,7 +17,7 @@
 </div>
 
 @if($footer)
-<div class="py-3 px-6 border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300 rounded-lg">
+<div class="mt-8 py-3 px-6 border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300 rounded-lg">
     <div class="flex flex-row items-center justify-between">
         {{ $footer }}
     </div>
