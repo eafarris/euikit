a552fe31db6eff05037f6a7eb6bb22e8aa7045ee
Eric Farris <eric@eafarris.com>
2024-01-01 15:14:48 -0500

    Consistent naming of fields in form.checkbox.

--- views/components/form/checkbox.blade.php
+++ views/components/form/checkbox.blade.php
@@ -1,6 +1,6 @@
-@props(['label', 'name'])
-<div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }}>
-    <input type="checkbox" name="{{ $name }}"
+@props(['label', 'field'])
+<div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }} id="{{ $field }}">
+    <input type="checkbox" name="{{ $field }}"
         {{ $attributes->whereStartsWith('wire') }}
     >
     <span class="ml-2 text-sm text-slate-500">{{ $label }}</span>
