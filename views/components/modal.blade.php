a168b3099e7269231695c7d247bc46a4f9423fd1
Eric Farris <eric@eafarris.com>
2024-02-09 16:15:09 -0500

    Fixed flashing, better styles in modal component

--- views/components/modal.blade.php
+++ views/components/modal.blade.php
@@ -1,7 +1,8 @@
 @props(['name'])
-<div id="{{ $name }}"
+<div id="{{ $name }}" 
     x-data="{ show: false, name: '{{ $name }}' }"
     x-show="show" 
+    x-cloak
     @keydown.escape.window="show=false"
     @modal.window="
         show = ($event.detail === name);
@@ -11,21 +12,21 @@
         @click="show = false"
     >
     </div><!-- backdrop -->
-    <div {{ $attributes->merge(['class' => "bg-slate-50/70 shadow-2xl p-4 m-auto w-fit h-fit fixed inset-0 border-slate-300 bg-gradient-to-br from-transparent via-slate-200 to-80% to-slate-100/50 dark:border-slate-900 dark:from-slate-600 dark:to-slate-700 border-2 rounded-2xl"]) }} >
+    <div {{ $attributes->merge(['class' => "z-50 bg-slate-50/70 shadow-2xl p-4 m-auto w-fit h-fit fixed inset-0 border-slate-300 bg-gradient-to-br from-transparent via-slate-200 to-80% to-slate-100/50 dark:border-slate-900 dark:from-slate-600 dark:to-slate-700 border-2 rounded-2xl"]) }} >
         <div class="flex flex-col h-full justify-between">
-            @if ($header)
+            @isset ($header)
                 <header class="py-3 px-6 border-2 border-slate-300 bg-slate-50/30 text-slate-500 text-2xl rounded-lg">
                     {{ $header }}
                 </header>
-            @endif
+            @endisset
             <div>
                 {{ $slot }}
             </div>
-            @if ($footer)
+            @isset ($footer)
                 <footer class="mt-8 py-3 px-6 border-2 border-slate-300 bg-transparent text-slate-500 rounded-lg">
                     {{ $footer }}
                 </footer>
-            @endif
+            @endisset
         </div>
     </div>
 </div>
