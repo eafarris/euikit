95e0ddba8c2039c2b6e215a71082fdd0acacf3a7
Eric Farris <eric@eafarris.com>
2024-02-09 16:11:26 -0500

    Sheet-Import LW component (!)

---
+++ src/Livewire/SheetImport.php
@@ -0,0 +1,57 @@
+<?php namespace eafarris\euikit\Livewire;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class SheetImport extends Component {
+    use WithFileUploads;
+
+    public $fields;
+    public $headerrow = TRUE;
+    public $importfield;
+    public $importfile;
+    public $type;
+
+    public function mount($fields = [], $type = 'button') {
+        $this->type = $type;
+        $this->fields = $fields;
+        $this->importfield = Str::wrap(implode('","', $fields), '"') . "\n";
+    }
+
+    public function render() {
+        return view('e::components.livewire.sheet-import');    
+    }
+
+    public function import() {
+        if (! $this->importfile) {  // No file, import string
+            $lines = explode(PHP_EOL, $this->importfield);
+        } // endif no file attached
+        else { // file uploaded
+            $path = $this->importfile->getRealPath();
+            $lines = file($path);
+        }
+        if ($this->headerrow) {
+            $headerrow = $lines[0];
+            $lines = Arr::except($lines, 0);
+        } // endif header row ()
+        $items = $this->parse($lines);
+        $this->dispatch('imported', importedItems: $items);
+    }
+
+    private function parse($csv) {
+        $items = collect();
+        foreach ($csv as $line) {
+            $line = trim($line);
+            $line_array = str_getcsv($line);
+            if (count($line_array) > 1) {
+                foreach ($this->fields as $index => $field) {
+                    $item[$field] = $line_array[$index];
+                }
+                $items->push($item);
+            }
+        }
+        return $items;
+    }
+}
---
+++ views/components/livewire/sheet-import.blade.php
@@ -0,0 +1,29 @@
+<div class="inline-block">
+@if ($type == 'button')
+    <x-e::button onclick="$modals.show('import')" value="Import to sheet" />
+@elseif ($type == 'action')
+    <x-e::form.sheet.action onclick="$modals.show('import')" icon="heroicon-c-document-arrow-down" title="Import" />
+@endif
+<pre>@php(var_dump($fields))@endphp</pre>
+
+@push('modals')
+    <x-e::modal name="import">
+        <x-slot:header>
+            Import to sheet
+        </x-slot:header>
+        <p class="py-4 text-slate-500">Select a CSV file for import, or paste text below.</p>
+        <x-e::form.checkbox class="py-4 px-8" field="headerrow" wire:model="headerrow" label="File/text contains a header row" />
+        <input class="px-8 text-slate-500
+            file:text-slate-500 file:border-transparent file:border file:font-normal file:text-base file:rounded file:py-1 file:px-3 file:h-12 file:flex-none file:justify-center file:items-center file:transition file:hover:scale-110
+            file:bg-slate-300 file:text-slate-800 file:hover:bg-slate-500 file:hover:text-white file:disabled:opacity-50"
+            type="file" name="importfile" wire:model="importfile" />
+        <x-e::form.textarea class="py-4" field="importfield" wire:model="importfield" label="Paste text" />
+        <x-slot:footer>
+            <div class="flex justify-between">
+                <x-e::button type="danger" @click="show = false" value="Close" />
+                <x-e::button type="success" @click="show = false" wire:click="import" value="Import" />
+            </div>
+        </x-slot:footer>
+    </x-e::modal>
+@endpush
+</div>
