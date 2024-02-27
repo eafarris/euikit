5519935d0f89f691beb04d63b350c536b2333cc9
Eric Farris <eric@eafarris.com>
2024-02-27 10:28:34 -0500

    Default coloring for left side of card-2column.

--- views/components/card-2column.blade.php
+++ views/components/card-2column.blade.php
@@ -1,6 +1,6 @@
 <section {{ $attributes->merge(['class' => "bg-white text-slate-800 dark:bg-slate-500 dark:text-slate-300 px-4 py-5 shadow sm:rounded-lg sm:p-6"]) }}>
 <div class="md:grid md:grid-cols-3 md:gap-6">
-    <div class="md:col-span-1"><!-- Left Column -->
+    <div class="md:col-span-1 text-slate-500"><!-- Left Column -->
         {{ $left }}
     </div><!-- Left Column -->
     <div class="mt-5 space-y-6 md:col-span-2 md:mt-0"><!-- Right Column -->
