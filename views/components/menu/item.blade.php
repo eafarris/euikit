f8dbf49417d9e53a70863103ca0b118a870d3de2
Eric Farris <eric@eafarris.com>
2024-02-28 13:18:25 -0500

    Support for badges in menu.item

--- views/components/menu/item.blade.php
+++ views/components/menu/item.blade.php
@@ -1,4 +1,4 @@
-@props(['icon', 'route', 'badge'])
+@props(['icon', 'route', 'badge', 'badge_style'])
 @php
 $classes = 'shrink-1 flex items-center p-2 pr-0 text-slate-500 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:rounded-bl-lg';
 if ($route == url()->current()) {
@@ -11,7 +11,7 @@ if ($route == url()->current()) {
         @svg($icon, 'w-6 h-6 pr-1 inline')
     @endisset
 {{ $slot }}
-    @isset($badge)
-    <x-e::tag type="{{ $badge_style}}">{{ $badge }}</x-e::tag>
-    @endisset
+    @if($badge != 0)
+    <x-e::tag type="{{ $badge_style }}">{{ $badge }}</x-e::tag>
+    @endif
 </a>
