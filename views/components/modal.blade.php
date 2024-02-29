0719e07f71cb63686655d6387dbf2076a1b342ce
Eric Farris <eric@eafarris.com>
2024-02-29 09:34:03 -0500

    More fixes for modal component.

--- views/components/modal.blade.php
+++ views/components/modal.blade.php
@@ -12,14 +12,14 @@
         @click="show = false"
     >
     </div><!-- backdrop -->
-    <div {{ $attributes->merge(['class' => "z-50 bg-slate-50/70 shadow-2xl p-4 m-auto size-fit fixed inset-0 border-slate-300 bg-gradient-to-br from-transparent via-slate-200 to-80% to-slate-100/50 dark:border-slate-900 dark:from-slate-600 dark:to-slate-700 border-2 rounded-2xl"]) }} >
+    <div {{ $attributes->merge(['class' => "z-50 bg-slate-50/70 shadow-2xl p-4 m-auto w-1/2 h-fit fixed inset-0 border-slate-300 bg-gradient-to-br from-transparent via-slate-200 to-80% to-slate-100/50 dark:border-slate-900 dark:from-slate-600 dark:to-slate-700 border-2 rounded-2xl"]) }} >
         <div class="flex flex-col size-fit justify-between">
             @isset ($header)
                 <header class="py-3 px-6 border-2 border-slate-300 bg-slate-50/30 text-slate-500 text-2xl rounded-lg">
                     {{ $header }}
                 </header>
             @endisset
-            <div>
+            <div class="py-8">
                 {{ $slot }}
             </div>
             @isset ($footer)
