3f6a7b578eaac78041b185932acf8b9df5c0a67b
Eric Farris <eric@eafarris.com>
2024-02-29 13:47:00 -0500

    Support for optional field if wire:model exists.

--- views/components/form/checkbox.blade.php
+++ views/components/form/checkbox.blade.php
@@ -1,4 +1,9 @@
 @props(['label', 'field'])
+@php
+if ($attributes->whereStartsWith('wire:model') && !isset($field)) {
+    $field = $attributes->whereStartsWith('wire:model')->first();
+}
+@endphp
 <div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }} id="{{ $field }}">
     <input type="checkbox" name="{{ $field }}"
         class="h-4 w-4 rounded border-slate-300 text-slate-500 focus:ring-indigo-600"
