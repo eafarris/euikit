2af990b464a049b9a2f3650913f6675ef6a50e3f
Eric Farris <eric@eafarris.com>
2024-03-25 10:15:53 -0400

    Fixed bug where LW comments would display in form.notes.

--- views/components/form/notes.blade.php
+++ views/components/form/notes.blade.php
@@ -1,4 +1,5 @@
 @props(['model' => '', 'label' => ''])
+
 <div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }}>
     <label for="notes" class="block text-sm font-medium text-slate-500 dark:text-slate-300 bg-transparent">
         @empty($label)
@@ -8,15 +9,21 @@
         @endempty
     </label>
     <div class="mt-1">
+    @if (!$attributes->whereStartsWith('wire'))
+<textarea 
+    class="resize-none appearance-none block text-slate-700 dark:bg-slate-400 dark:text-slate-700 border border-slate-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-slate-500 {{ $errors->has('notes') ? 'is-danger' : ''}} "
+    cols="60" rows="10" name="notes" id="notes"
+>
+{{ $model == '' ?: $model->notes }}
+</textarea>
+@else
 <textarea 
     class="resize-none appearance-none block text-slate-700 dark:bg-slate-400 dark:text-slate-700 border border-slate-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-slate-500 {{ $errors->has('notes') ? 'is-danger' : ''}} "
     cols="60" rows="10" name="notes" id="notes"
     {{ $attributes->whereStartsWith('wire') }}
 >
-@if (!$attributes->whereStartsWith('wire'))
-{{ $model == '' ?: $model->notes }}
-@endif
 </textarea>
+@endif
     </div>
     @error('notes')
         <p class="mt-2 text-sm text-red-600">{{ $message }}</p><!-- .mt-2 text-sm text-red-600 -->
