3e566ba00b0aa78c09152bb7d92af8ce66186601
Eric Farris <eric@eafarris.com>
2024-04-04 10:20:32 -0400

    Wrong colors in form.boolean labels

--- views/components/form/boolean.blade.php
+++ views/components/form/boolean.blade.php
@@ -12,11 +12,11 @@ if ($attributes->whereStartsWith('wire:model') && !isset($field)) {
         <div class="flex mt-1">
             <div class="flex items-center mb-4 mr-3">
                 <input class="h-4 w-4 border-slate-300 focus:ring-2 focus:ring-indigo-500" type="radio" name="{{ $field }}" id="{{ $field }}_yes" value="1" {{ $value ? 'checked' : '' }} {{ $attributes->whereStartsWith('wire') }}>
-                <label for="{{ $field}}_yes" class="text-sm font-medium text-slate-900 ml-2 block">Yes</label>
+                <label for="{{ $field}}_yes" class="text-sm font-medium text-slate-500 ml-2 block">Yes</label>
             </div>
             <div class="flex items-center mb-4">
                 <input class="h-4 w-4 border-slate-300 focus:ring-2 focus:ring-indigo-500" type="radio" name="{{ $field }}" id="{{ $field }}_no" value="0" {{ $value ? '' : 'checked' }} {{ $attributes->whereStartsWith('wire')}}>
-                <label for="{{ $field}}_no" class="text-sm font-medium text-slate-900 ml-2 block">No</label>
+                <label for="{{ $field}}_no" class="text-sm font-medium text-slate-500 ml-2 block">No</label>
             </div>
         </div>
     @else
