7244bae8f4564f094d73f6f2b664654edb95541c
Eric Farris <eric@eafarris.com>
2024-05-03 13:10:08 -0400

    Normalized types and colors in tag component.

--- views/components/tag.blade.php
+++ views/components/tag.blade.php
@@ -1,9 +1,13 @@
+@props(['type' => 'info'])
 <span @class([
+    'bg-slate-100 text-slate-500'=> $type == 'light',
+    'bg-slate-500 text-slate-100' => $type == 'dark',
+    'bg-transparent text-blue-600' => $type == 'link',
+    'bg-slate-300 text-slate-900' => $type == 'info',
     'bg-yellow-300 text-yellow-900' => $type == 'warning' || $type == 'yellow',
     'bg-green-300 text-green-900' => $type == 'success' || $type == 'green',
-    'bg-red-300 text-red-900' => $type == 'error' || $type == 'danger' || $type == 'red', 
+    'bg-red-300 text-red-900' => $type == 'error' || $type == 'danger' || $type == 'delete' || $type == 'red',
     'bg-blue-300 text-blue-900' => $type == 'primary' || $type == 'blue',
-    'bg-slate-300 text-slate-900' => $type == 'info' || $type == 'slate',
     'inline-block align-middle text-xs uppercase font-semibold px-2 py-1 rounded-md mx-1'
 ])>
 {{ $slot }}
