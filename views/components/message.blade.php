48c50d5e90b50a3d551521940c26e6ba0fc3b3d9
Eric Farris <eric@eafarris.com>
2024-05-03 16:28:24 -0400

    Rewritten message component.

--- views/components/message.blade.php
+++ views/components/message.blade.php
@@ -1,71 +1,51 @@
-@props(['type' => 'info', 'title' => '', 'icon' => ''])
+@props(['type' => 'info', 'inline' => FALSE, 'lefticon' => '', 'righticon' => '',
+])
 @php
-    switch($type) {
-        case 'warning':
-        case 'error':
-            $icon = $icon == 'none' ? '' : 'heroicon-o-exclamation-circle';
-            break;
-        case 'success':
-            $icon = $icon == 'none' ? '' : 'heroicon-o-check-circle';
-            break;
-        default:
-            $icon = $icon == 'none' ? '' : $icon;
-            break;
-    }
-
 $coloring = '';
-if ($type) {
-    switch ($type) {
-        case 'warning':
-            $topcoloring = 'bg-yellow-200 border-yellow-400';
-            $bottomcoloring = 'bg-yellow-50 border-yellow-400';
-            $textcoloring = 'text-yellow-500';
-            break;
-        case 'success':
-            $topcoloring = 'bg-green-200 border-green-400';
-            $bottomcoloring = 'bg-green-50 border-green-400';
-            $textcoloring = 'text-green-500';
-            break;
-        case 'error':
-            $topcoloring = 'bg-red-200 border-red-400';
-            $bottomcoloring = 'bg-red-50 border-red-400';
-            $textcoloring = 'text-red-500';
-            break;
-        case 'primary':
-            $topcoloring = 'bg-blue-200 border-blue-400';
-            $bottomcoloring = 'bg-blue-50 border-blue-400';
-            $textcoloring = 'text-blue-500';
-            break;
-        case 'info':
-            $topcoloring = 'bg-slate-200 border-slate-400';
-            $bottomcoloring = 'bg-slate-50 border-slate-400';
-            $textcoloring = 'text-slate-500';
-            break;
-    }
-}
+switch ($type) {
+    case 'light':
+      $coloring = 'bg-slate-100 text-slate-500 ';
+      break;
+    case 'dark':
+      $coloring = 'bg-slate-500 text-slate-100';
+      break;
+    case 'link':
+      $coloring = 'bg-transparent text-blue-600';
+      break;
+    case 'info':
+      $coloring = 'bg-slate-100 text-slate-700';
+      break;
+    case 'success':
+    case 'green':
+      $coloring = 'bg-green-100 text-green-700';
+      break;
+    case 'warning':
+    case 'yellow':
+      $coloring = 'bg-yellow-100 text-yellow-700';
+      break;
+    case 'danger':
+    case 'delete':
+    case 'error':
+    case 'red':
+      $coloring = 'bg-red-100 text-red-700';
+      break;
+    case 'primary':
+    case 'blue':
+      $coloring = 'bg-blue-100 text-blue-700';
+      break;
+    case 'ghost':
+      $coloring = 'bg-transparent text-slate-400';
+      break;
+    default:
+        $lefticon = $lefticon == 'none' ? '' : $icon;
+        break;
+  }
 @endphp
-<div {{ $attributes->merge(['class' => 'bg-transparent '])}}>
-@if ($title)
-    <div class="p-4 rounded-t-xl border {{ $topcoloring }}">
-        <p class="{{ $textcoloring }} font-bold tracking-wide">
-        @if ($icon)
-            <span>
-                @svg($icon, 'w-8 h-8 inline')
-            </span>
-        @endif
-            <span class="align-text-top">
-                {{ $title }}
-            </span>
-        </p>
-    </div>
-@endif
-    <div @class([
-        'p-4 rounded-t-xl border-t' => !$title,
-        'pl-4 pb-4 pt-2 rounded-b-xl border-b border-l border-r ' . $bottomcoloring,
-        ])
-    >
-        <p class="mt-1 font-normal align-text-top {{ $textcoloring }}">
-            {{ $slot }}
-        </p>
-    </div>
-</div><!-- EUIKit Message Component -->
+<span @class([
+    'inline-flex' => $inline,
+    'flex' => !$inline,
+    'gap-1 grow-0 shrink w-fit px-1 py-1 text-sm rounded',
+    $coloring,
+])>
+    {{ $slot }}
+</span>
