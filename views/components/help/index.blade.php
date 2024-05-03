1b6f7a6a16697959b12bb332f00a843e284eadae
Eric Farris <eric@eafarris.com>
2024-05-03 16:28:37 -0400

    Help component uses message component internally.

--- views/components/help/index.blade.php
+++ views/components/help/index.blade.php
@@ -1,15 +1,7 @@
 @props(['type' => 'ghost'])
-<aside x-show="euikit_help_show" @class([
-    'bg-slate-100 text-slate-500'=> $type == 'light',
-    'bg-slate-500 text-slate-100' => $type == 'dark',
-    'bg-transparent text-blue-600' => $type == 'link',
-    'bg-slate-100 text-slate-900' => $type == 'info',
-    'bg-yellow-100 text-yellow-900' => $type == 'warning' || $type == 'yellow',
-    'bg-green-100 text-green-900' => $type == 'success' || $type == 'green',
-    'bg-red-100 text-red-900' => $type == 'error' || $type == 'danger' || $type == 'delete' || $type == 'red',
-    'bg-blue-100 text-blue-900' => $type == 'primary' || $type == 'blue',
-    'bg-transparent text-slate-400' => $type == 'ghost',
-    'text-sm italic euikit-help px-2'
-])>
-    {{ $slot }}
+
+<aside x-show="euikit_help_show">
+    <x-e::message type="{{ $type }}">
+        <span class="italic">{{ $slot }}</span>
+    </x-e::message>
 </aside>
