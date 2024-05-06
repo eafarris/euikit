c33c761a1e5a93b314c261aae0cb700a258af7fe
Eric Farris <eric@eafarris.com>
2024-05-06 11:10:50 -0400

    Added border support to message component.

--- views/components/message.blade.php
+++ views/components/message.blade.php
@@ -1,40 +1,40 @@
-@props(['type' => 'info', 'inline' => FALSE, 'lefticon' => '', 'righticon' => '',
+@props(['type' => 'info', 'inline' => FALSE, 'lefticon' => '', 'righticon' => '', 'border' => FALSE,
 ])
 @php
 $coloring = '';
 switch ($type) {
     case 'light':
-      $coloring = 'bg-slate-100 text-slate-500 ';
+      $coloring = 'bg-slate-100 text-slate-400 border-slate-300';
       break;
     case 'dark':
-      $coloring = 'bg-slate-500 text-slate-100';
+      $coloring = 'bg-slate-500 text-slate-100 border-slate-600';
       break;
     case 'link':
-      $coloring = 'bg-transparent text-blue-600';
+      $coloring = 'bg-transparent text-blue-600 border-blue-400';
       break;
     case 'info':
-      $coloring = 'bg-slate-100 text-slate-700';
+      $coloring = 'bg-slate-100 text-slate-700 border-slate-400';
       break;
     case 'success':
     case 'green':
-      $coloring = 'bg-green-100 text-green-700';
+      $coloring = 'bg-green-100 text-green-700 border-green-500';
       break;
     case 'warning':
     case 'yellow':
-      $coloring = 'bg-yellow-100 text-yellow-700';
+      $coloring = 'bg-yellow-100 text-yellow-700 border-yellow-500';
       break;
     case 'danger':
     case 'delete':
     case 'error':
     case 'red':
-      $coloring = 'bg-red-100 text-red-700';
+      $coloring = 'bg-red-100 text-red-700 border-red-500';
       break;
     case 'primary':
     case 'blue':
-      $coloring = 'bg-blue-100 text-blue-700';
+      $coloring = 'bg-blue-100 text-blue-700 border-blue-500';
       break;
     case 'ghost':
-      $coloring = 'bg-transparent text-slate-400';
+      $coloring = 'bg-transparent text-slate-400 border-slate-200';
       break;
     default:
         $lefticon = $lefticon == 'none' ? '' : $icon;
@@ -45,6 +45,7 @@ switch ($type) {
     'inline-flex' => $inline,
     'flex' => !$inline,
     'gap-1 grow-0 shrink w-fit px-1 py-1 text-sm rounded',
+    'border-rounded-sm border-2' => $border,
     $coloring,
 ])>
     {{ $slot }}
