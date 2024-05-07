f648cdbdff9ea7c946a7153ce3f3d56919e48c08
Eric Farris <eric@eafarris.com>
2024-05-07 14:41:50 -0400

    Support for icons in section component.

--- views/components/section.blade.php
+++ views/components/section.blade.php
@@ -1,15 +1,22 @@
-@props(['header' => '', 'subheader' => '', 'footer' => ''])
+@props(['header' => '', 'footer' => '',
+    'leftheadericon' => '', 'rightheadericon' => '',
+    'leftfootericon' => '', 'rightfootericon' => '',
+])
 <section {{ $attributes->merge(['class' => 'min-w-fit m-12 border-slate-300 bg-gradient-to-br from-slate-100 via-slate-200 via-10% to-80% to-slate-100 dark:border-slate-900 dark:from-slate-600 dark:to-slate-700 border-2 p-6 rounded-lg shadow-2xl']) }}>
 @if($header)
 <div class="py-3 px-6 border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300 rounded-lg">
-    <div class="flex flex-row items-center justify-between text-2xl">
+    <div class="flex flex-row gap-1 grow-0 shrink items-center justify-items-center text-2xl">
+        @if($leftheadericon)
+            @svg($leftheadericon, 'w-8 h-8 mr-4')
+        @endif
         {{ $header }}
+        @if($rightheadericon)
+            <div class="w-fit flex grow justify-end">
+                @svg($rightheadericon, 'w-8 h-8 ml-4')
+            </div>
+        @endif
     </div>
 </div>
-@endif
-
-@if($subheader)
-<p class="pt-2 pl-1 text-sm text-slate-600 dark:text-slate-400 italic">{{ $subheader}}</p>
 @endif
 
 <div class="text-lg text-slate-700 dark:text-slate-300">
@@ -19,7 +26,13 @@
 @if($footer)
 <div class="mt-8 py-3 px-6 border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300 rounded-lg">
     <div class="flex flex-row items-center justify-between">
+        @if($leftfootericon)
+            @svg($leftfootericon, 'w-8 h-8 mr-4')
+        @endif
         {{ $footer }}
+        @if($rightfootericon)
+            @svg($rightfootericon, 'w-8 h-8 ml-4')
+        @endif
     </div>
 </div>
 @endif
