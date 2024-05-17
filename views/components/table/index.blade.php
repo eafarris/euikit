d9fa6d5b8b148739f7b808034eab8bf9824958c3
Eric Farris <eric@eafarris.com>
2024-05-17 13:01:30 -0400

    Tables are full-width by default.

--- views/components/table/index.blade.php
+++ views/components/table/index.blade.php
@@ -1,5 +1,5 @@
 <div class="sm:rounded-lg border-2 border-slate-200 dark:border-slate-900 overflow-hidden mt-4 mb-4">
-<table {{ $attributes->merge(['class' => 'divide-y divide-slate-300 dark:divide-slate-700 table-fixed']) }}>
+<table {{ $attributes->merge(['class' => 'divide-y divide-slate-300 dark:divide-slate-700 table-fixed w-full']) }}>
     @if (isset($header))
         <thead class="bg-slate-100 dark:bg-slate-600 sticky top-0">
             {{ $header }}
