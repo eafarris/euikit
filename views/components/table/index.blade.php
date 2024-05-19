3e8fe7af3bd664ab95897fe739f201fc526f915e
Eric Farris <eric@eafarris.com>
2024-05-19 09:53:54 -0400

    Some transparency under table headers when scrolling.

--- views/components/table/index.blade.php
+++ views/components/table/index.blade.php
@@ -1,7 +1,7 @@
 <div class="sm:rounded-lg border-2 border-slate-200 dark:border-slate-900 overflow-hidden mt-4 mb-4">
 <table {{ $attributes->merge(['class' => 'divide-y divide-slate-300 dark:divide-slate-700 table-fixed w-full']) }}>
     @if (isset($header))
-        <thead class="bg-slate-100 dark:bg-slate-600 sticky top-0">
+        <thead class="bg-slate-100/95 dark:bg-slate-600/95 sticky top-0">
             {{ $header }}
         </thead>
     @endif
