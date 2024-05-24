f02db6df6b4cb7b6ff5e7968f59dcc22820c6d2c
Eric Farris <eric@eafarris.com>
2024-05-24 13:41:04 -0400

    Removed obnoxious comments in card-2column component

--- views/components/card-2column.blade.php
+++ views/components/card-2column.blade.php
@@ -1,10 +1,10 @@
 <section {{ $attributes->merge(['class' => "bg-white text-slate-800 dark:bg-slate-500 dark:text-slate-300 px-4 py-5 shadow sm:rounded-lg sm:p-6"]) }}>
 <div class="md:grid md:grid-cols-3 md:gap-6">
-    <div class="md:col-span-1 text-slate-500"><!-- Left Column -->
+    <div class="md:col-span-1 text-slate-500">
         {{ $left }}
-    </div><!-- Left Column -->
-    <div class="mt-5 space-y-6 md:col-span-2 md:mt-0"><!-- Right Column -->
+    </div>
+    <div class="mt-5 space-y-6 md:col-span-2 md:mt-0">
         {{ $right }}
-    </div><!-- Right Column -->
+    </div>
 </div>
-</section><!-- EUIKit Two-Column Card component -->
+</section>
