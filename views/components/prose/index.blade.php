082617ca16ba4e146cb02a289cdd6669d838e477
Eric Farris <eric@eafarris.com>
2024-05-29 13:44:16 -0400

    Increased tracking in prose component

--- views/components/prose/index.blade.php
+++ views/components/prose/index.blade.php
@@ -1,3 +1,3 @@
-<div class="lg:w-1/2 m-8 font-serif font-medium text-lg text-slate-900 leading-relaxed">
+<div class="lg:w-1/2 m-8 font-serif font-medium text-lg text-slate-900 leading-relaxed tracking-tight">
     {{ $slot }}
 </div>
