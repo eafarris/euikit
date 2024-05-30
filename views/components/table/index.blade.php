a22f8e20cb2090797c22686addb45b52ec127771
Eric Farris <eric@eafarris.com>
2024-05-30 13:07:19 -0400

    Support for 'nohover' in table component

--- views/components/table/index.blade.php
+++ views/components/table/index.blade.php
@@ -1,3 +1,4 @@
+@props(['nohover' => FALSE])
 <div class="sm:rounded-lg border-2 border-slate-200 dark:border-slate-900 overflow-hidden mt-4 mb-4">
 <table {{ $attributes->merge(['class' => 'divide-y divide-slate-300 dark:divide-slate-700 table-fixed w-full']) }}>
     @if (isset($header))
