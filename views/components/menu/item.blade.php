a12dd73b4e250af0a44ce1a780f8dacf79e99d34
Eric Farris <eric@eafarris.com>
2024-06-01 14:01:22 -0400

    Resorting to trickery to see if route == current url.

--- views/components/menu/item.blade.php
+++ views/components/menu/item.blade.php
@@ -1,19 +1,22 @@
-@props(['icon', 'route', 'badge', 'badge_style'])
+@props(['route', 'icon' => '', 'badge' => '', 'badgeStyle' => ''])
 @php
-$classes = 'shrink-1 flex items-center p-2 pr-0 text-slate-500 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:rounded-bl-lg';
-if ($route == url()->current()) {
-    $classes = 'shrink-1 flex items-center p-2 pr-0 text-slate-500 bg-slate-50 dark:text-slate-300 dark:bg-slate-800 rounded-tl-lg rounded-bl-lg';
-}
+    // Laravel's URL() function never takes hashes into account
+    $part_of_url = \Str::before(url()->current(), '#') == \Str::before($route, '#');
 @endphp
-
-<a href="{{ $route }}" {{ $attributes->merge(['class' => $classes]) }}>
+<a href="{{ $route }}" 
+    @class([
+        'shrink-1 flex items-center p-2 pr-0 text-slate-500 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:rounded-bl-lg',
+        'dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:roundedl-bl-lg' => !$part_of_url,
+        'bg-slate-50 dark:text-slate-300 dark:bg-slate-800' => $part_of_url,
+    ])
+>
     @isset($icon)
         @svg($icon, 'w-6 h-6 pr-1 inline')
     @endisset
 {{ $slot }}
     @isset($badge)
         @unless($badge == '' || $badge == 0)
-        <x-e::tag type="{{ $badge_style }}">{{ $badge }}</x-e::tag>
+        <x-e::tag type="{{ $badgeStyle }}">{{ $badge }}</x-e::tag>
         @endunless
     @endisset
 </a>
