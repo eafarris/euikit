9459f0194f1869a19d0c0ef4c0921340b7e3b7b6
Eric Farris <eric@eafarris.com>
2024-06-05 13:56:50 -0400

    Header and Footer back to slots in section; better support for icons in header and footer.

--- views/components/section.blade.php
+++ views/components/section.blade.php
@@ -1,12 +1,19 @@
-@props(['header' => '', 'footer' => '',
-    'leftheadericon' => '', 'rightheadericon' => '',
-    'leftfootericon' => '', 'rightfootericon' => '',
-])
+@props(['header' => '', 'footer' => '', ])
 <section {{ $attributes->merge(['class' => 'min-w-fit m-12 border-slate-300 bg-gradient-to-br from-slate-100 via-slate-200 via-10% to-80% to-slate-100 dark:border-slate-900 dark:from-slate-600 dark:to-slate-700 border-2 p-6 rounded-lg shadow-2xl']) }}>
+
 @if($header)
-    <x-e::header lefticon="{{ $leftheadericon }}" righticon="{{ $rightheadericon }}">
+    @unless(is_string($header))
+    <x-e::header
+        {{ $attributes->merge([
+            'lefticon' => $header->attributes['lefticon'],
+            'righticon' => $header->attributes['righticon']
+        ]) }}
+    >
         {{ $header }}
     </x-e::header>
+    @else
+        <x-e::header> {{ $header }} </x-e::header>
+    @endunless
 @endif
 
 <div class="text-lg text-slate-700 dark:text-slate-300">
@@ -14,7 +21,12 @@
 </div>
 
 @if($footer)
-    <x-e::footer lefticon="{{ $leftfootericon }}" righticon="{{ $rightfootericon }}">
+    <x-e::footer
+        {{ $attributes->merge([
+            'lefticon' => $footer->attributes['lefticon'],
+            'righticon' => $footer->attributes['righticon']
+        ]) }}
+    >
         {{ $footer }}
     </x-e::footer>
 @endif
