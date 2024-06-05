8e6a206ba84184dd5bd96732b8f969efec89d91b
Eric Farris <eric@eafarris.com>
2024-06-05 13:56:07 -0400

    Added missing margin to footer component.

--- views/components/footer.blade.php
+++ views/components/footer.blade.php
@@ -4,7 +4,7 @@
  --}}
  @props(['lefticon' => '', 'righticon' => ''])
 
- <div {{ $attributes->merge(['class' => 'py-3 px-6 rounded-lg border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300']) }}>
+ <div {{ $attributes->merge(['class' => 'mt-4 py-3 px-6 rounded-lg border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300']) }}>
     <div class="flex flex-row gap-4 grow-0 shrink items-center justify-between text-xl">
         @if($lefticon)
             @svg($lefticon, 'w-8 h-8 mr-4')
