8902bf346660282d3234435797b601b2ce55b354
Eric Farris <eric@eafarris.com>
2024-06-05 08:25:36 -0400

    Better test for current url == route

--- views/components/menu/item.blade.php
+++ views/components/menu/item.blade.php
@@ -1,18 +1,19 @@
 @props(['route', 'icon' => '', 'badge' => '', 'badgeStyle' => ''])
 @php
     // Laravel's URL() function never takes hashes into account
-    $part_of_url = \Str::before(url()->current(), '#') == \Str::before($route, '#');
+    $part_of_url = \Str::before(url()->current(), '#') == \Str::before($route, '#') ||
+        url()->current() === url($route);
 @endphp
-<a href="{{ $route }}" 
+<a href="{{ $route }}"
     @class([
         'shrink-1 flex items-center p-2 pr-0 text-slate-500 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:rounded-bl-lg',
         'dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 hover:rounded-tl-lg hover:roundedl-bl-lg' => !$part_of_url,
         'bg-slate-50 dark:text-slate-300 dark:bg-slate-800' => $part_of_url,
     ])
 >
-    @isset($icon)
+    @if($icon)
         @svg($icon, 'w-6 h-6 pr-1 inline')
-    @endisset
+    @endif
 {{ $slot }}
     @isset($badge)
         @unless($badge == '' || $badge == 0)
