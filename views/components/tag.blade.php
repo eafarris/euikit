0fbe66a4b2050de3b34c3662d52fd09e575bb367
Eric Farris <eric@eafarris.com>
2024-06-07 11:01:51 -0400

    Rework of styling in tag component.

--- views/components/tag.blade.php
+++ views/components/tag.blade.php
@@ -2,34 +2,50 @@
     'route' => '',
     'lefticon' => '', 'righticon' => '',
 ])
+@php
+    $tag = 'inline-flex gap-1 grow-0 shrink items-center justify-items-center text-xs uppercase font-semibold px-2 py-1 rounded-md mx-1';
+
+    $coloring = '';
+    switch ($type) {
+        case 'light':
+            $coloring = 'bg-slate-100 text-slate-500';
+            break;
+        case 'dark':
+            $coloring = 'bg-slate-500 text-slate-100';
+            break;
+        case 'link':
+            $coloring = 'bg-transparent text-blue-600 underline underline-offset-2';
+            break;
+        case 'info':
+            $coloring = 'bg-slate-300 text-slate-900';
+            break;
+        case 'success':
+        case 'green':
+            $coloring = 'bg-green-300 text-green-900';
+            break;
+        case 'warning':
+        case 'yellow':
+            $coloring = 'bg-yellow-300 text-yellow-900';
+            break;
+        case 'danger':
+        case 'delete':
+        case 'error':
+        case 'red':
+            $coloring = 'bg-red-300 text-red-900';
+            break;
+        case 'primary':
+        case 'blue':
+            $coloring = 'bg-blue-300 text-blue-900';
+            break;
+        case 'ghost':
+            $coloring = 'bg-transparent text-slate-400';
+            break;
+    } // endswitch
+@endphp
 @if ($route)
-<a @class([
-    'bg-slate-100 text-slate-500'=> $type == 'light',
-    'bg-slate-500 text-slate-100' => $type == 'dark',
-    'bg-transparent text-blue-600 underline underline-offset-2' => $type == 'link',
-    'bg-slate-300 text-slate-900' => $type == 'info',
-    'bg-yellow-300 text-yellow-900' => $type == 'warning' || $type == 'yellow',
-    'bg-green-300 text-green-900' => $type == 'success' || $type == 'green',
-    'bg-red-300 text-red-900' => $type == 'error' || $type == 'danger' || $type == 'delete' || $type == 'red',
-    'bg-blue-300 text-blue-900' => $type == 'primary' || $type == 'blue',
-    'bg-transparent text-slate-400' => $type == 'ghost',
-    'inline-flex gap-1 grow-0 shrink items-center justify-items-center text-xs uppercase font-semibold px-2 py-1 rounded-md mx-1'
-])
-href="{{ $route }}"
->
+    <a href="{{ $route }}" {{ $attributes->merge(['class' => $tag . ' ' . $coloring]) }}>
 @else
-<span @class([
-    'bg-slate-100 text-slate-500'=> $type == 'light',
-    'bg-slate-500 text-slate-100' => $type == 'dark',
-    'bg-transparent text-blue-600 underline underline-offset-2' => $type == 'link',
-    'bg-slate-300 text-slate-900' => $type == 'info',
-    'bg-yellow-300 text-yellow-900' => $type == 'warning' || $type == 'yellow',
-    'bg-green-300 text-green-900' => $type == 'success' || $type == 'green',
-    'bg-red-300 text-red-900' => $type == 'error' || $type == 'danger' || $type == 'delete' || $type == 'red',
-    'bg-blue-300 text-blue-900' => $type == 'primary' || $type == 'blue',
-    'bg-transparent text-slate-400' => $type == 'ghost',
-    'inline-flex gap-1 grow-0 shrink items-center justify-items-center text-xs uppercase font-semibold px-2 py-1 rounded-md mx-1'
-])>
+<span {{ $attributes->merge(['class' => $tag . ' ' . $coloring]) }}>
 @endif
     @if ($lefticon)
         @svg($lefticon, 'w-3 h-3 mr-1')
