8f70e8b847fe6e835f7a3f04900f68357f334f9d
Eric Farris <eric@eafarris.com>
2024-06-07 11:19:00 -0400

    Add dark mode to nohover on table component

--- views/components/table/row.blade.php
+++ views/components/table/row.blade.php
@@ -1,7 +1,7 @@
 @aware(['nohover' => FALSE])
 <tr @class([
-    'hover:bg-slate-100' => ! $nohover
+    'hover:bg-slate-100 dark:hover:bg-slate-800' => ! $nohover
 ])
-{{ $attributes->merge(['class' => "bg-white dark:bg-slate-500 dark:hover:bg-slate-800 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"]) }}>
+{{ $attributes->merge(['class' => "bg-white dark:bg-slate-500 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"]) }}>
     {{ $slot }}
 </tr><!-- EUIKit Table Row -->
