d0f6252de2e6d6249ed291eace7d581f62b6e471
Eric Farris <eric@eafarris.com>
2024-11-08 08:23:46 -0500

    Header component should be a <header>

--- views/components/header.blade.php
+++ views/components/header.blade.php
@@ -4,7 +4,7 @@
  --}}
  @props(['lefticon' => '', 'righticon' => ''])
 
- <div {{ $attributes->merge(['class' => 'py-3 px-6 rounded-lg border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300']) }}>
+ <header {{ $attributes->merge(['class' => 'py-3 px-6 rounded-lg border-2 border-slate-300 bg-slate-50/50 text-slate-500 dark:border-slate-700 dark:bg-slate-500 dark:text-slate-300']) }}>
     <div class="flex flex-row items-center justify-between text-2xl">
         @if($lefticon)
             @svg($lefticon, 'w-8 h-8 mr-4')
@@ -16,4 +16,4 @@
             @svg($righticon, 'w-8 h-8 ml-4')
         @endif
     </div>
-</div>
+</header>
