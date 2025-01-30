baf5f2160ac86029db4b41fd4cea481acf6983ad
Eric Farris <eric@eafarris.com>
2025-01-30 13:31:46 -0500

    Fixed missing spaces in class declaration

--- views/components/form/sheet/display.blade.php
+++ views/components/form/sheet/display.blade.php
@@ -1,8 +1,8 @@
 @props(['value' => ''])
 @php
-  $classes = 'p-2 text-base self-baseline tabular-nums border line-clamp-1 hover:line-clamp-none';
+  $classes = 'p-2 text-base self-baseline tabular-nums border line-clamp-1 hover:line-clamp-none ';
   $classes .= 'border-slate-200 bg-slate-50 ';
-  $classes .= 'dark:border-slate-500 dark:bg-slate-800 dark:text-slate-300';
+  $classes .= 'dark:border-slate-500 dark:bg-slate-800 dark:text-slate-300 ';
 @endphp
 <div {{ $attributes->merge(['class' => $classes]) }} >
     {{ $value }}
