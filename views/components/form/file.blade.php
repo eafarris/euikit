db7d937ada61bf34c8e3b5c28d9e57dd74dbcae5
Eric Farris <eric@eafarris.com>
2025-09-26 10:50:33 -0400

    Better flex display on form.file component

--- views/components/form/file.blade.php
+++ views/components/form/file.blade.php
@@ -2,7 +2,7 @@
     'help' => '', 'helptype' => 'ghost',
 ])
 <!-- Based on https://codepen.io/jjjrmy/pen/gOPvmdv?editors=1010 -->
-<div class="flex flex-col flex-grow mb-3 w-64 h-64 p-4 ">
+<div class="flex flex-col grow mb-3 w-64 h-64 p-4 ">
     <div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field w-60 h-60']) }}
         x-data="{ files: null }"
     >
