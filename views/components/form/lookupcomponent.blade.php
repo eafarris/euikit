a5142c8c6ed77390c41d19d108d6fd43a3535d2f
Eric Farris <eric@eafarris.com>
2025-09-26 10:51:34 -0400

    Lookup component now uses euikit select component

--- views/components/form/lookupcomponent.blade.php
+++ views/components/form/lookupcomponent.blade.php
@@ -1,43 +1,19 @@
-@props(['field', 'label' => '', 'nolabel' => FALSE, 'value' => '',
-    'any' => FALSE, 'none' => FALSE, 'models', 'optionvalue' => 'id', 'optionfield' => '',
-    'help' => '', 'helptype' => 'ghost', 'nonevalue' => 0, 'anyvalue' => 'any',
+ @props(['field', 'label' => '', 'nolabel' => FALSE, 'value' => '', 'any' => FALSE, 'none' => FALSE, 'noplaceholder' => FALSE, 'placeholder' => '', 'multi' => FALSE,
+    'help' => '', 'helptype' => 'ghost',
 ])
-
-@php
-    $common_classes = 'block min-h-8 w-full pl-2 rounded-sm shadow-xs sm:text-sm border leading-tight appearance-none';
-    $color_classes = 'bg-white text-slate-500 dark:text-slate-700 dark:bg-slate-400 border-slate-200 dark:border-slate-700 focus:ring-sky-300 focus:border-sky-300 focus:outline-hidden';
-    $error_classes = 'border-red-500 text-red-500;'
-@endphp
-<div {{ $attributes->merge(['class' => 'field']) }}>
-    @unless($nolabel)
-        <label for="{{ $field }}" class="block text-sm mb-2 font-medium text-slate-500 dark:text-slate-300 bg-transparent">
-            {{ $label ?: ucfirst($field) }}
-        </label>
-    @endunless
-    <div class="relative mt-1">
-        <div class="select">
-            <select
-              name="{{ $field }}"
-              {{ \Arr::except($attributes, 'class') }}
-              @class([$common_classes,
-                $color_classes => ! $errors->has($field),
-                $error_classes => $errors->has($field),
-              ])
-            >
-            <option disabled value="">Select {{ $label ?: ucfirst($field) }}</option>
-            @if($any)
-                <option value="{{ $anyvalue }}">Any</option>
-            @endif
-            @if($none)
-                <option value="{{ $nonevalue }}">None</option>
-            @endif
-            @foreach ($models as $model)
-                <option value="{{ $model->$optionvalue }}" {{ $value == $model->$optionvalue ? "selected" : "" }}>{{ $model->$optionfield }}</option>
-            @endforeach
-            </select>
-        </div>
-        @isset($help)
-            <x-euikit::help>{{ $help }}</x-euikit::help>
-        @endisset
-    </div>
-</div><!-- EUIKit Form Lookup component -->
+<x-euikit::form.select {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'lookup']) }}
+    field="{{ $field }}" label="{{ $label }}" nolabel="{{ $nolabel }}" value="{{ $value }}"
+    any="{{ $any }}" none="{{ $none }}" anyvalue="{{ $anyvalue }}" nonevalue="{{ $nonevalue }}"
+    noplaceholder="{{ $noplaceholder }}" multi="{{ $multi }}"
+    help="{{ $help }}" helptype="{{ $helptype }}"
+>
+    @if ($any)
+        <option value="{{ $anyvalue }}">Any</option>
+    @endif
+    @if ($none)
+        <option value="{{ $nonevalue }}">None</option>
+    @endif
+    @foreach ($models as $model)
+        <option value="{{ $model->$optionvalue }}" {{ $value == $model->$optionvalue ? "selected" : ''}}>{{ $model->$optionfield }}</option>
+    @endforeach
+</x-euikit::form.select>
