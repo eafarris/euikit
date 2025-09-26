e74843df152b1d8901a0c81ac733ed112f706a63
Eric Farris <eric@eafarris.com>
2025-09-26 10:52:14 -0400

    Better rounding of form.notes component

--- views/components/form/notes.blade.php
+++ views/components/form/notes.blade.php
@@ -3,7 +3,7 @@
 ])
 
 @php
-    $common_classes = 'resize-none appearance-none block border rounded-sm py-3 p-4 mb-3 leading-tight';
+    $common_classes = 'resize-none appearance-none block border rounded-xs py-3 p-4 mb-3 leading-tight';
     $color_classes = 'border-slate-300 text-slate-500 bg-white/50 focus:border-sky-300 focus:ring-sky-300 placeholder:text-slate-400 ';
     $color_classes .= 'dark:border-slate-400 dark:bg-slate-700 dark:text-slate-300 dark:placeholder:text-slate-400 dark:focus:border-sky-600 dark:focus:ring-sky-600';
     $error_classes = 'border-red-500 text-red-900';
