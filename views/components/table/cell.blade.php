f165559958b2fe90a4d6635c4ac4d8838b34fecb
Eric Farris <eric@eafarris.com>
2025-09-26 10:59:25 -0400

    Better padding of table.cell component

--- views/components/table/cell.blade.php
+++ views/components/table/cell.blade.php
@@ -1,4 +1,4 @@
-<td {{ $attributes->merge(['class' => "w-full lg:w-auto p-3 text-slate-600 dark:text-slate-300 lg:table-cell relative lg:static"]) }}>
+<td {{ $attributes->merge(['class' => "w-full lg:w-auto p-2 text-slate-600 dark:text-slate-300 lg:table-cell relative lg:static"]) }}>
     @if(isset($label))
         <span class="lg:hidden absolute top-0 left-0 bg-blue-100 px-2 py-1 text-xs font-bold uppercase">{{ $label }}</span>
     @endif
