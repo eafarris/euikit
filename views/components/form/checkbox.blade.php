886d5993e930acf00c4a843178ddc6342630a57f
Eric Farris <eric@eafarris.com>
2025-09-26 10:46:26 -0400

    Better rounding of form.checkbox component

--- views/components/form/checkbox.blade.php
+++ views/components/form/checkbox.blade.php
@@ -9,7 +9,7 @@ if ($attributes->whereStartsWith('wire:model') && !isset($field)) {
 <div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }} id="{{ $field }}">
     <label class="flex items-center gap-4 text-sm text-slate-500">
         <input type="checkbox" name="{{ $field }}"
-            class="h-4 w-4 rounded-sm border-slate-300 text-slate-500 focus:ring-sky-300"
+            class="h-4 w-4 rounded-xs border-slate-300 text-slate-500 focus:ring-sky-300"
             {{ $attributes->whereStartsWith('wire') }}
         >
         {{ $label ?: ucfirst($field)}}
