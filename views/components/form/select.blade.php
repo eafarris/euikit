ad72f95aaeb44d2720092131d5a650a9599f2350
Eric Farris <eric@eafarris.com>
2025-11-06 14:16:10 -0500

    form.select: Don't show dropdown chevron if in multiselect mode

--- views/components/form/select.blade.php
+++ views/components/form/select.blade.php
@@ -1,5 +1,5 @@
 @props(['field', 'label' => '', 'nolabel' => FALSE, 'value' => '', 'noplaceholder' => FALSE, 'placeholder' => '', 'multi' => FALSE,
-    'any' => FALSE, 'anyvalue' => 0, 'none' => FALSE, 'nonevalue' => 0,
+    'any' => FALSE, 'anyvalue' => 0, 'none' => FALSE, 'nonevalue' => NULL,
     'help' => '', 'helptype' => 'ghost',
 ])
 @php
@@ -9,22 +9,24 @@
     }
 
     $common_classes = 'block p-2 pr-8 w-full rounded-xs shadow-2xs sm:text-sm border leading-tight appearance-none';
-    $color_classes = 'border-slate-300 text-slate-500 bg-transparent focus:border-sky-300 focus:ring-sky-300 placeholder:text-slate-400 ';
+    $color_classes = 'border-slate-300 text-slate-600 bg-transparent focus:border-sky-300 focus:ring-sky-300 placeholder:text-slate-400 ';
     $color_classes .= 'dark:border-slate-400 dark:bg-slate-700 dark:text-slate-300 dark:placeholder:text-slate-400 dark:focus:border-sky-600 dark:focus:ring-sky-600';
     $error_classes = 'border-red-500 text-red-900';
 @endphp
 
 <div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }}>
     @unless($nolabel)
-        <label for="{{ $field }}" class="block text-sm mb-2 font-medium text-slate-500 dark:text-slate-300 bg-transparent">
+        <label for="{{ $field }}" class="block text-sm mb-2 font-medium text-slate-400 dark:text-slate-300 bg-transparent">
             {{ $label ?: ucfirst($field) }}
         </label>
     @endunless
     <div class="relative mt-1">
         <div class="select">
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center">
-                @svg('heroicon-o-chevron-down', 'w-5 h-5 text-slate-400')
-            </div>
+            @unless ($multi)
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center">
+                    @svg('heroicon-o-chevron-down', 'w-5 h-5 text-slate-400')
+                </div>
+            @endunless
             <select
             {{ $attributes->whereStartsWith('wire') }}
             @if ($multi) multiple @endif
