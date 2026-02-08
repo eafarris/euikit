86f07f2a1aec04a446d31b4e0503a8c860035650
Eric Farris <eric@eafarris.com>
2026-02-08 13:37:38 -0500

    Notes now uses textarea component, markdown is optional

--- views/components/form/notes.blade.php
+++ views/components/form/notes.blade.php
@@ -1,5 +1,5 @@
-@props(['model' => '', 'label' => '',
-    'help' => '', 'helptype' => 'ghost',
+@props(['model' => '', 'label' => '', 'cols' => 60, 'rows' => 10, 'nomarkdown' => FALSE,
+    'labelhtml' => '', 'help' => '', 'helptype' => 'ghost',
 ])
 
 @php
@@ -7,43 +7,15 @@
     $color_classes = 'border-slate-300 text-slate-500 bg-white/50 focus:border-sky-300 focus:ring-sky-300 placeholder:text-slate-400 ';
     $color_classes .= 'dark:border-slate-400 dark:bg-slate-700 dark:text-slate-300 dark:placeholder:text-slate-400 dark:focus:border-sky-600 dark:focus:ring-sky-600';
     $error_classes = 'border-red-500 text-red-900';
+
+    $label = 'Notes';
+    if (!$nomarkdown) {
+      $labelhtml .= 'Notes <span class="italic">(<a href="https://www.markdownguide.org/cheat-sheet/" class="underline-offset-2 underline">Markdown</a> supported)</span>';
+    }
 @endphp
 
-<div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }}>
-    <label for="notes" class="block text-sm font-medium text-slate-500 dark:text-slate-300 bg-transparent">
-        @empty($label)
-            Notes <span class="italic">(<a href="https://www.markdownguide.org/cheat-sheet/" class="underline-offset-2 underline">Markdown</a> supported)</span>
-        @else
-            {{ $label }}
-        @endempty
-    </label>
-    <div class="mt-1">
-    @if (!$attributes->whereStartsWith('wire'))
-<textarea
-    @class([$common_classes,
-        $color_classes => ! $errors->has('notes'),
-        $error_classes => $errors->has('notes'),
-    ])
-    cols="60" rows="10" name="notes" id="notes"
+<x-euikit::form.textarea
+  labelhtml="{!! $labelhtml !!}"
+  field="notes" cols="{{ $cols }}" rows="{{ $rows }}"
 >
-{{ $model == '' ?: $model->notes }}
-</textarea>
-@else
-<textarea
-    @class([$common_classes,
-        $color_classes => ! $errors->has('notes'),
-        $error_classes => $errors->has('notes'),
-    ])
-    cols="60" rows="10" name="notes" id="notes"
-    {{ $attributes->whereStartsWith('wire') }}
->
-</textarea>
-@endif
-    </div>
-    @isset($help)
-        <x-euikit::help type="{{ $helptype }}">{{ $help }}</x-euikit::help>
-    @endisset
-    @error('notes')
-        <p class="mt-2 text-sm text-red-600">{{ $message }}</p><!-- .mt-2 text-sm text-red-600 -->
-    @enderror
-</div><!-- EUIKit Notes field -->
+</x-euikit::form.textarea>
