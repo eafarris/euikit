e8d558a25a5411acdf1b10931d3b00e0b0f8acd0
Eric Farris <eric@eafarris.com>
2026-02-08 13:35:45 -0500

    Rewrite of boolean component, supporting full-item select and LW

--- views/components/form/boolean.blade.php
+++ views/components/form/boolean.blade.php
@@ -24,35 +24,57 @@ if ($choices) {
             break;
     } // endswitch choices
 }
+
+$label_colors = 'bg-transparent text-slate-500 dark:text-slate-300 rounded-lg border border-transparent ';
+$label_colors .= 'has-checked:bg-sky-50 has-checked:border-sky-500 has-checked:text-sky-700';
+$choice_classes = 'text-sm font-medium ml-2 block ' . $label_colors;
+$radio_classes = 'peer appearance-none col-start-1 row-start-1 size-4 rounded-full border-1 outline-1';
+$radio_classes .= 'border-slate-400 bg-transparent ';
+$radio_classes .= 'hover:border-slate-500 hover:bg-slate-100 ';
+$radio_classes .= 'dark:border-slate-700 dark:text-slate-700';
+$checked_classes = 'col-start-1 row-start-1 flex items-center rounded-full hidden peer-checked:block text-slate-400';
+$error_classes = 'border-red-500 text-red-500'
+
 @endphp
 
 <div {{ $attributes->merge(['class' => 'field', 'p-4', 'flex items-center gap-4']) }} {{ $attributes->whereDoesntStartWith('wire') }}>
     @if ($type == 'radio')
-        <label for="{{ $field }}" class="block text-sm mb-2 font-medium text-slate-500 dark:text-slate-300 bg-transparent">{{ $label == '' ? ucfirst($field) : $label }}</label>
+        <label for="{{ $field }}" class="block text-sm mb-2 font-medium {{ $label_colors }}">{{ $label == '' ? ucfirst($field) : $label }}</label>
         <div class="flex items-center gap-4">
-            <div class="flex">
-                <input class="h-4 w-4 border-slate-300 text-slate-500 focus:ring-2 focus:ring-sky-300
-                    dark:border-slate-700 dark:text-slate-700 dark:focus:ring-sky-600 "
-                    type="radio" name="{{ $field }}" id="{{ $field }}_yes" value="1" {{ $value ? 'checked' : '' }} {{ $attributes->whereStartsWith('wire') }}>
-                <label for="{{ $field}}_yes" class="text-sm font-medium text-slate-500 dark:text-slate-300 ml-2 block">{{ $choicetrue }}</label>
+            <div class="flex items-center">
+                <div class="grid place-items-center">
+                    <input class="{{ $radio_classes }}"
+                        type="radio" name="{{ $field }}" id="{{ $field }}_yes" value="1" {{ $value ? 'checked' : '' }} {{ $attributes->whereStartsWith('wire') }}>
+                <div class="{{ $checked_classes }}">@svg('heroicon-s-check-circle', 'size-6')</div>
+                </div>
+                <label for="{{ $field}}_yes" class="{{ $choice_classes }}">{{ $choicetrue }}</label>
             </div>
-            <div class="flex">
-                <input class="h-4 w-4 border-slate-300 text-slate-500 focus:ring-2 focus:ring-sky-300
-                    dark:border-slate-700 dark:text-slate-700 dark:focus:ring-sky-600 "
-                    type="radio" name="{{ $field }}" id="{{ $field }}_no" value="0" {{ $value ? '' : 'checked' }} {{ $attributes->whereStartsWith('wire')}}>
-                <label for="{ $field}}_no" class="text-sm font-medium text-slate-500 dark:text-slate-300 ml-2 block">{{ $choicefalse }}</label>
+            <div class="flex items-center">
+                <div class="grid place-items-center">
+                    <input class="{{ $radio_classes }}"
+                        type="radio" name="{{ $field }}" id="{{ $field }}_no" value="0" {{ $value ? '' : 'checked' }} {{ $attributes->whereStartsWith('wire')}}>
+                    <div class="{{ $checked_classes }}">@svg('heroicon-s-check-circle', 'size-6')</div>
+                </div>
+                <label for="{ $field}}_no" class="{{ $choice_classes }}">{{ $choicefalse }}</label>
             </div>
         </div>
     @else
-        <div class="p-4 block text-sm font-medium text-slate-500 bg-transparent">
-        <input type="checkbox" name="{{ $field }}"
-            {{ $attributes->whereStartsWith('wire') }}
-            class="h-6 w-6 rounded-xs border-slate-300 text-slate-500 focus:ring-sky-300 mr-2"
-            @if ($value) checked @endif
-        /> <span class="text-slate-500 dark:text-slate-300">{{ $label == '' ? ucfirst($field) : $label }}</span>
-        </div>
+        <label class="p-4 flex items-center text-sm font-medium {{ $label_colors }}">
+            <input type="checkbox" name="{{ $field }}"
+                {{ $attributes->whereStartsWith('wire') }}
+                class="h-6 w-6 rounded-xs border-slate-300 text-slate-500 focus:ring-sky-300 mr-2"
+                @if ($value) checked @endif
+                @unless ($attributes->whereStartsWith('wire'))
+                @endunless
+            />
+            {{ $label }}
+        </label>
     @endif
     @isset($help)
         <x-euikit::help type="{{ $helptype }}">{{ $help }}</x-euikit::help>
     @endisset
+    @error($field)
+        <x-euikit::message type="error">{{ $message }}</x-euikit::message>
+    @enderror
+
 </div><!-- EUIKit Boolean field -->
