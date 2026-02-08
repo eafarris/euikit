d1f027c02d64453855854bf27c203011d0721563
Eric Farris <eric@eafarris.com>
2026-02-08 13:28:37 -0500

    LW support for datetime form component.

--- views/components/form/datetime.blade.php
+++ views/components/form/datetime.blade.php
@@ -8,8 +8,8 @@ if ($attributes->whereStartsWith('wire:model') && !isset($field)) {
     $field = $attributes->whereStartsWith('wire:model')->first();
 }
 
-$common_classes = 'block w-1/6 min-w-[200px] rounded-md shadow-2xs sm:text-sm border leading-tight appearance-none placeholder:italic';
-$color_classes = 'border-slate-300 text-slate-500 bg-transparent focus:border-sky-300 focus:ring-sky-300 placeholder:text-slate-400 ';
+$common_classes = 'w-full min-h-8 rounded-xs shadow-2xs sm:text-sm border leading-tight appearance-none placeholder:italic pl-2';
+$color_classes = 'border-slate-300 text-slate-600 bg-transparent focus:border-sky-300 focus:ring-sky-300 placeholder:text-slate-300 ';
 $color_classes .= 'dark:border-slate-400 dark:bg-slate-700 dark:text-slate-300 dark:placeholder:text-slate-400 dark:focus:border-sky-600 dark:focus:ring-sky-600';
 $error_classes = 'border-red-500 text-red-500';
 
@@ -22,27 +22,26 @@ if (!empty($value)) {
 
 <div {{ $attributes->whereDoesntStartWith('wire')->merge(['class' => 'field']) }} id="{{ $field }}">
     @unless($nolabel)
-        <label for="{{ $field }}" class="block text-sm font-medium text-slate-500 dark:text-slate-300 bg-transparent">
+        <label for="{{ $field }}" class="block mb-2 text-sm font-medium text-slate-400 dark:text-slate-300 bg-transparent">
             {{ $label ?: ucfirst($field) }}
         </label>
     @endunless
-    <div class="relative">
-        <input type="datetime-local"
-            id="{{ $field }}"
-            name="{{ $field }}"
-            value="{{ @old($field, $value) }}"
-            @class([$common_classes,
-                $color_classes => ! $errors->has($field),
-                $error_classes => $errors->has($field),
-            ])
-            @if ($required) required @endif
-            @if ($min) min="{{ $min }}" @endif
-            @if ($max) max="{{ $max }}" @endif
-        />
-        @isset($help)
-            <x-euikit::help type="{{ $helptype }}">{{ $help }}</x-euikit::help>
-        @endisset
-    </div><!-- .control -->
+    <input type="datetime-local"
+        id="{{ $field }}"
+        name="{{ $field }}"
+        value="{{ @old($field, $value) }}"
+        @class([$common_classes,
+            $color_classes => ! $errors->has($field),
+            $error_classes => $errors->has($field),
+        ])
+        @if ($required) required @endif
+        @if ($min) min="{{ $min }}" @endif
+        @if ($max) max="{{ $max }}" @endif
+        {{ $attributes->whereStartsWith('wire') }}
+    />
+    @isset($help)
+        <x-euikit::help type="{{ $helptype }}">{{ $help }}</x-euikit::help>
+    @endisset
 
     @error($field)
         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
